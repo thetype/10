5006baaa6f692f8e6103842f4bc6b04b59f3cc20
Zhengyu Qian <laerhsif@gmail.com>
2017-06-11 05:12:46 +0800

    Remove unused private members

--- src/javascript/class/slideshow.js
+++ src/javascript/class/slideshow.js
@@ -7,8 +7,8 @@ TIB.Slideshow = class _Slideshow {
 		this.$prev  = this.$el.querySelector(selector.prev);
 		this.$next  = this.$el.querySelector(selector.next);
 		this.index  = 0;
-		this.slideW = this.$list.children[0].offsetWidth;
-		this.slideH = this.$list.children[0].offsetHeight;
+		// this.slideW = this.$list.children[0].offsetWidth;
+		// this.slideH = this.$list.children[0].offsetHeight;
 		
 		this.startX = -1;
 		this.startY = -1;
