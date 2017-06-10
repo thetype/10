40689fa320b01eb619e2c9e58609ff3202005b62
Zhengyu Qian <laerhsif@gmail.com>
2017-06-10 20:09:18 +0800

    Update global defines for TIB

--- src/javascript/_define.js
+++ src/javascript/_define.js
@@ -1,20 +1,13 @@
 /* global doT, wx */
 
-const ENV = 'production';
-
-// Baidu statistics
-let _hmt = _hmt || [];
+const ENV = ENV || 'production';
 
 // Global namespace for the New Money Selected app
 const TIB = TIB || {};
 
 /*
 TIB.API_DOMAIN =
-	typeof gon === 'object' && gon.env === 'production'
-	? ''
-	: 'http://test.xinqian.me';
-*/
-TIB.API_DOMAIN =
 	ENV === 'production'
-	? 'http://xinqian.me'
-	: 'http://test.xinqian.me';
+	? 'http://thetype.com'
+	: 'http://test.thetype.com';
+*/
